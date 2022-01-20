cdd695512d51b68ec0da8a89cc4c5d8c919f54c1
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-19 19:57:49 -0800

    commit tree ef6964128e9735de8c7a73e037f237232a0fac87

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        # line_start = "The line is currently:"
+        # katz_deli.each.with_index {|person,index| line_start << " #{index + 1}. #{person}"}
+        # puts line_start
+        puts "The line is currently:#{(katz_deli.map.with_index {|person,index| " #{index + 1}. #{person}"}).join}"
+    end
+
+end
+
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        next_person = katz_deli.shift
+        puts "Currently serving #{next_person}."
+    end
+end
